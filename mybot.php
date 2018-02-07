c4b551e55d0aa908490aa43e002e0158c0126dec
bbonimu <36217339+bbonimu@users.noreply.github.com>
2018-02-07 11:21:46 +0700

    Update mybot.php

--- mybot.php
+++ mybot.php
@@ -29,6 +29,13 @@ if (!is_null($events['events'])) {
 					'text' => 'ทดสอบๆ'
 				];
 			}
+			else if($text == 'BNK48'){
+				// Build message to reply back
+				$messages = [
+					'type' => 'text',
+					'text' => 'โอนิกิริ'
+				];
+			}
 		
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
